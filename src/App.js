e30a23c479db15274638b3fd4683910fd0326c06
Shrewbitz <swxg@umich.edu>
2020-11-08 16:29:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super() 
+       this.state = {
+         string: "hey sammy"
+       }
+    
+  }
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              whats up utubedfsdfd
+            </a>
+            <button onClick={()=> this.setState({string: "Hey bill"})}>{this.state.string}</button>
+          </header>
+        </div>
+      );
+  }
 }
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           whats up utubedd
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
